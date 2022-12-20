6e3ddf765d48206be23e61bcb79b1d24defb0b80
Guilherme Dourado <guiihdourado@gmail.com>
2022-12-20 16:33:25 -0300

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://docs.sorteio.com/'
+      },
+      {
+        source: "/docs/(.*?)([.]\\w{2,5})(\\?[^/]+)?",
+        destination: "https://docs.sorteio.com/$1$2$3"
+      },
+      {
+        source: "/docs/([\\S\\s]+)",
+        destination: "https://docs.sorteio.com/$1/"
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
